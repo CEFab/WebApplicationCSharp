93de5fc9f0220174976f8c20173de22d6d837bd7
fabio <fabio@fabio.com>
2024-06-08 18:43:37 -0400

    initial commit

---
+++ WebApplication1/Models/Articulo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class Articulo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        [ForeignKey("Autor")]
+        public int IdAutor { get; set; }
+        public Autor Autor { get; set; }
+        public string Contenido { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+
+
+
+    }
+}
